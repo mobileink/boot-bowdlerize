af5108f791d970282e5696b2eeea34ae0db0a783
Gregg Reynolds <reynolds-gregg@norc.org>
2016-02-22 21:14:27 -0600

    add another bower pkg to jetty example

--- jetty/config/bower.clj
+++ jetty/config/bower.clj
@@ -1,7 +1,8 @@
 (ns bower)
 
 (def config-map
-  {"materialize" [{:ns 'hello.styles :name 'materialize
+  {"moment" 'hello.scripts/moment
+   "materialize" [{:ns 'hello.styles :name 'materialize
                    :uri "bower_components/Materialize/bin/materialize.css"}
                   {:ns 'hello.scripts :name 'materialize
                    :uri "bower_components/Materialize/bin/materialize.js"}]})
