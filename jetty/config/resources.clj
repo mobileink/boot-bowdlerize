21405adbd1968853fbda51c8d947842c7c544320
Gregg Reynolds <reynolds-gregg@norc.org>
2016-02-23 07:17:53 -0600

    minor tweak

--- jetty/config/resources.clj
+++ jetty/config/resources.clj
@@ -5,8 +5,7 @@
    {:runtime 'hello.scripts/app :uri "scripts/app.js"}])
 
 (def statics
-  [{:runtime 'hello.resources/statics
-    :uri "target"}])
+  [{:runtime 'hello.resources/statics :uri "target"}])
 
 (def styles
   [{:runtime 'hello.styles/app :uri "styles/app.css"}])
